ba626e0ae8612149304e4c7f9d9b915ca3a14b51
RoznBoy <rozn@seoultech.ac.kr>
2024-03-19 11:29:04 +0900

    s4_3190

--- g4_3190.py
+++ g4_3190.py
@@ -3,3 +3,9 @@ input = sys.stdin.readline
 
 N = int(input())
 K = int(input())
+for _ in range(K):
+    x, y = map(int,input().split())
+    
+L = int(input())
+for _ in range(L):
+    s, c = map(int,input().split())
