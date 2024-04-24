e4fc584f3f71f3d55dbcf16ca94c4314a461fb91
RoznBoy <rozn@seoultech.ac.kr>
2024-04-24 23:34:30 +0900

    8주차 DP

--- s3_2503.py
+++ s3_2503.py
@@ -1,7 +1,9 @@
+from itertools import permutations
 import sys
 input = sys.stdin.readline
 
 N = int(input())
 
+baseball=list(permutations([1,2,3,4,5,6,7,8,9],3))
 for _ in range(N):
     q, s, b  = map(int, input().split())
